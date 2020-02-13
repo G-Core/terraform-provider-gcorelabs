25ad67e0ea19c5e306cb8f2ee5bc21246370f8c3
Gentyuk, Valentin <vgentyuk@itkey.com>
2020-02-13 15:09:27 +0300

    create and delete a volume

--- task.go
+++ task.go
@@ -11,32 +11,27 @@ import (
 
 func get_resp(url string, token string) (map[string]interface{}) {
 	// do a request
-	client := &http.Client{Timeout: 10 * time.Second}
+	client := &http.Client{Timeout: TIMEOUT_SEC * time.Second}
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Add("Authorization", token)
 	log.Printf("do request %s, %s", req, err)
 	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
+	check_err(err)
 
 	// get a response body as text
 	responseData, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	check_err(err)
 	var data map[string]interface{}
 	log.Printf("RD%s", responseData)
-	error := json.Unmarshal([]byte(responseData), &data)
+	err = json.Unmarshal([]byte(responseData), &data)
 	log.Printf("RD%s", data)
-	if error != nil {
-		panic(error)
-	}
+	check_err(err)
 	return data
 }
 
 func task_wait(task_id string, token string) (interface{}) {
-	url := fmt.Sprintf("http://localhost:8888/v1/tasks/%s", task_id)
+	log.Printf("Start of waiting a task %s", task_id)
+	url := fmt.Sprintf("%stasks/%s", HOST, task_id)
 	timeout := 180
 	pause := 5
 	for i := 0; i < timeout / pause; i++{
@@ -46,14 +41,18 @@ func task_wait(task_id string, token string) (interface{}) {
 		}
 		if resp_data["state"] == "FINISHED"{
 			log.Printf("The task %s finished", resp_data["id"])
+			log.Printf("Finish of waiting a task %s", task_id)
 			return resp_data["created_resources"]
 		}
 	}
+	log.Printf("Start waiting a task %s", task_id)
 	return nil
 }
 
-type Stype struct{
-	Size int `json:"size"`
-	Source string `json:"source"`
-	Name string	`json:"name"`
+func full_task_wait(resp *http.Response, token string) (interface{}) {
+	task := new(Task)
+	err := json.NewDecoder(resp.Body).Decode(task)
+	check_err(err)
+	task_id := task.Tasks[0]
+	return task_wait(task_id, token)
 }
