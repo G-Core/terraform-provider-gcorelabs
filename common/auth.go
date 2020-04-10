ffeeb6bb1c4f1fd5b9e1e5f677a9a444961cad3d
valentin g <33gva@mail.ru>
2020-04-10 16:42:58 +0300

    delete an excess struct

--- common/auth.go
+++ common/auth.go
@@ -4,11 +4,6 @@ import (
 	"bitbucket.gcore.lu/gcloud/gcorecloud-go"
 )
 
-type auth struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
 type Config struct {
 	Host     string // if project and region requests will be replaced, delete this field
 	Timeout  int    // if project and region requests will be replaced, delete this field
