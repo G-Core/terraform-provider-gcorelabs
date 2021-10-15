14213c8ca7dd9bc7b6c31a2ae3990e1459fd836b
Alexandr Gorshunov <a.gorshunov@andersenlab.com>
2021-10-15 11:37:03 +0700

    gcorelabscloud-go version updated, instance example fixed

--- examples/resources/gcore_instance/resource.tf
+++ examples/resources/gcore_instance/resource.tf
@@ -92,4 +92,59 @@ resource "gcore_instance" "instance" {
   project_id = 1
 }
 
+//***
+// another one example with one interface to private network and fip to internet
+//***
+
+resource "gcore_reservedfixedip" "fixed_ip" {
+  project_id = 1
+  region_id = 1
+  type = "ip_address"
+  network_id = "faf6507b-1ff1-4ebf-b540-befd5c09fe06"
+  fixed_ip_address = "192.168.13.6"
+  is_vip = false
+}
+
+resource "gcore_volume" "first_volume" {
+  name = "boot volume"
+  type_name = "ssd_hiiops"
+  size = 10
+  image_id = "6dc4e061-6fab-41f3-91a3-0ba848fb32d9"
+  project_id = 1
+  region_id = 1
+}
+
+resource "gcore_floatingip" "fip" {
+  project_id = 1
+  region_id = 1
+  fixed_ip_address = gcore_reservedfixedip.fixed_ip.fixed_ip_address
+  port_id = gcore_reservedfixedip.fixed_ip.port_id
+}
+
+
+resource "gcore_instance" "v" {
+  project_id = 1
+  region_id = 1
+  name = "hello"
+  flavor_id = "g1-standard-1-2"
+
+  volume {
+    source = "existing-volume"
+        volume_id = gcore_volume.first_volume.id
+        boot_index = 0
+  }
+  security_group {
+    id = "ada84751-fcca-4491-9249-2dfceb321616"
+    name = "default"
+  }
+
+  interface {
+    type = "reserved_fixed_ip"
+        port_id = gcore_reservedfixedip.fixed_ip.port_id
+        fip_source = "existing"
+        existing_fip_id = gcore_floatingip.fip.id
+  }
+}
+
+
 
