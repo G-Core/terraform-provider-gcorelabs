e950b9d32b47dee8dbddeabfd280c141c6c243fe
Vasil Mikhalenya <v_mikhalenya@gcore.lu>
2022-01-20 17:51:08 +0100

    DNS examples updated

--- examples/resources/gcore_dns_zone_record/resource.tf
+++ examples/resources/gcore_dns_zone_record/resource.tf
@@ -7,6 +7,36 @@ provider gcore {
   gcore_dns_api = "https://dnsapi.gcorelabs.com"
 }
 
+//
+// example0: managing zone and records by TF using variables
+//
+variable "example_domain0" {
+  type    = string
+  default = "examplezone.com"
+}
+
+resource "gcore_dns_zone" "examplezone0" {
+  name = var.example_domain0
+}
+
+resource "gcore_dns_zone_record" "example_rrset0" {
+  zone = gcore_dns_zone.examplezone0.name
+  domain = "${gcore_dns_zone.examplezone0.name}"
+  type = "A"
+  ttl = 100
+
+  resource_record {
+    content = "127.0.0.100"
+  }
+  resource_record {
+    content = "127.0.0.200"
+    // enabled = false
+  }
+}
+
+//
+// example1: managing zone outside of TF 
+//
 resource "gcore_dns_zone_record" "subdomain_examplezone" {
   zone = "examplezone.com"
   domain = "subdomain.examplezone.com"
