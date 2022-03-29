93516808d2b85d93a242a5775bb5b2d516da9b4f
alexk53 <98004722+alexk53@users.noreply.github.com>
2022-03-29 16:58:52 +0700

    gore_loadbalancerv2 resource and data-source added (#89)
    gcore_loadbalancer resource and data-source deprecated
    
    Co-authored-by: Alexandr Sokolov <asokolov@itkey.com>

---
+++ gcore/resource_gcore_loadbalancerv2.go
@@ -0,0 +1,223 @@
+package gcore
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/G-Core/gcorelabscloud-go/gcore/loadbalancer/v1/loadbalancers"
+	"github.com/G-Core/gcorelabscloud-go/gcore/task/v1/tasks"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceLoadBalancerV2() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceLoadBalancerV2Create,
+		ReadContext:   resourceLoadBalancerV2Read,
+		UpdateContext: resourceLoadBalancerV2Update,
+		DeleteContext: resourceLoadBalancerDelete,
+		Description:   "Represent load balancer without nested listener",
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: func(ctx context.Context, d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
+				projectID, regionID, lbID, err := ImportStringParser(d.Id())
+
+				if err != nil {
+					return nil, err
+				}
+				d.Set("project_id", projectID)
+				d.Set("region_id", regionID)
+				d.SetId(lbID)
+
+				return []*schema.ResourceData{d}, nil
+			},
+		},
+
+		Schema: map[string]*schema.Schema{
+			"project_id": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+				ExactlyOneOf: []string{
+					"project_id",
+					"project_name",
+				},
+			},
+			"region_id": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+				ExactlyOneOf: []string{
+					"region_id",
+					"region_name",
+				},
+			},
+			"project_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				ExactlyOneOf: []string{
+					"project_id",
+					"project_name",
+				},
+			},
+			"region_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				ExactlyOneOf: []string{
+					"region_id",
+					"region_name",
+				},
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"flavor": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"vip_network_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"vip_subnet_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"vip_address": &schema.Schema{
+				Type:        schema.TypeString,
+				Description: "Load balancer IP address",
+				Computed:    true,
+			},
+			//todo fix client and enabled vip_port_id
+			//"vip_port_id": &schema.Schema{
+			//	Type: schema.TypeString,
+			//	Optional: true,
+			//  ForceNew: true,
+			//},
+			"last_updated": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceLoadBalancerV2Create(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Println("[DEBUG] Start LoadBalancer creating")
+	var diags diag.Diagnostics
+	config := m.(*Config)
+	provider := config.Provider
+
+	client, err := CreateClient(provider, d, LoadBalancersPoint, versionPointV1)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	opts := loadbalancers.CreateOpts{
+		Name:         d.Get("name").(string),
+		VipNetworkID: d.Get("vip_network_id").(string),
+		VipSubnetID:  d.Get("vip_subnet_id").(string),
+	}
+
+	lbFlavor := d.Get("flavor").(string)
+	if len(lbFlavor) != 0 {
+		opts.Flavor = &lbFlavor
+	}
+
+	results, err := loadbalancers.Create(client, opts).Extract()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	taskID := results.Tasks[0]
+	lbID, err := tasks.WaitTaskAndReturnResult(client, taskID, true, LoadBalancerCreateTimeout, func(task tasks.TaskID) (interface{}, error) {
+		taskInfo, err := tasks.Get(client, string(task)).Extract()
+		if err != nil {
+			return nil, fmt.Errorf("cannot get task with ID: %s. Error: %w", task, err)
+		}
+		lbID, err := loadbalancers.ExtractLoadBalancerIDFromTask(taskInfo)
+		if err != nil {
+			return nil, fmt.Errorf("cannot retrieve LoadBalancer ID from task info: %w", err)
+		}
+		return lbID, nil
+	})
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(lbID.(string))
+	resourceLoadBalancerV2Read(ctx, d, m)
+
+	log.Printf("[DEBUG] Finish LoadBalancer creating (%s)", lbID)
+	return diags
+}
+
+func resourceLoadBalancerV2Read(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Println("[DEBUG] Start LoadBalancer reading")
+	var diags diag.Diagnostics
+	config := m.(*Config)
+	provider := config.Provider
+
+	client, err := CreateClient(provider, d, LoadBalancersPoint, versionPointV1)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	lb, err := loadbalancers.Get(client, d.Id()).Extract()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	d.Set("project_id", lb.ProjectID)
+	d.Set("region_id", lb.RegionID)
+	d.Set("name", lb.Name)
+	d.Set("flavor", lb.Flavor.FlavorName)
+
+	if lb.VipAddress != nil {
+		d.Set("vip_address", lb.VipAddress.String())
+	}
+
+	fields := []string{"vip_network_id", "vip_subnet_id"}
+	revertState(d, &fields)
+
+	log.Println("[DEBUG] Finish LoadBalancer reading")
+	return diags
+}
+
+func resourceLoadBalancerV2Update(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Println("[DEBUG] Start LoadBalancer updating")
+	config := m.(*Config)
+	provider := config.Provider
+
+	client, err := CreateClient(provider, d, LoadBalancersPoint, versionPointV1)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if d.HasChange("name") {
+		opts := loadbalancers.UpdateOpts{
+			Name: d.Get("name").(string),
+		}
+		_, err = loadbalancers.Update(client, d.Id(), opts).Extract()
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		d.Set("last_updated", time.Now().Format(time.RFC850))
+	}
+
+	log.Println("[DEBUG] Finish LoadBalancer updating")
+	return resourceLoadBalancerV2Read(ctx, d, m)
+}
